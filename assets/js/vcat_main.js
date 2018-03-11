81fc89db0c5b6d1f95a4f77fd31fd0ff3c3853cc
Dmytro Tarasenko <itaysontesterlab@gmail.com>
2018-03-11 16:32:28 +0200

    0.2.1: proxy additions

--- assets/js/vcat_main.js
+++ assets/js/vcat_main.js
@@ -2,13 +2,13 @@
 var token = getItem("authToken");
 
 function getNews() {
-    /*$.ajax({
-        url: "http://SOSI/proxy.php?url=\"https://api.vk.com/method/newsfeed.get?access_token="+token+"&filters=post\"",
+    $.ajax({
+        url: "http://vcatclient.000webhostapp.com/proxy.php?method=getNewsfeed&token="+token,
         success: function( response ) {
             var result = JSON.parse(response);
             $.each(result['response']['items'],function(index, value){
                 console.log('My array has at position ' + index + ', this value: ' + value['text']);
             });
         }
-    });*/
+    });
 }
--- proxy.php
+++ proxy.php
@@ -4,8 +4,25 @@ header('Access-Control-Allow-Origin: *');
  * Base proxy
  * For testing + for Ukrainian users
  */
+$about = array(
+    'proxyName' => 'Offical VK Kitten Proxy',
+    'proxyContact' => 'https://github.com/HardSer/VCat',
+    'proxyAuthor' => 'iTaysonLab'
+);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$url = str_replace('"','',$_GET['url']);
-echo file_get_contents($url);
+$urlStart = 'https://api.vk.com/method/';
+switch ($_GET['method']) {
+    case 'getNewsfeed':
+        $urlEnd = 'newsfeed.get?access_token='.$_GET['token'].'&filters=post';
+        break;
+    case 'proxyInfo':
+        echo json_encode($about);
+        die();
+        break;
+    default:
+        die();
+        break;
+}
+echo file_get_contents($urlStart.$urlEnd);
