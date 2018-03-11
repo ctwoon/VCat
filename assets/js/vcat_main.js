f1104de80b6024fbf7b492552a17c6b42c1e6e6a
iTaysonLab <itaysontesterlab@gmail.com>
2018-03-11 19:05:30 +0200

    attachments support!

--- assets/js/vcat_main.js
+++ assets/js/vcat_main.js
@@ -1,6 +1,6 @@
 /** VCat Main **/
 var token = getItem("authToken");
-var debug = true;
+var debug = false;
 function getNews() {
     var url = "https://api.vk.com/method/newsfeed.get?access_token="+token+"&filters=post&v=5.73";
     if (!debug) {
@@ -14,13 +14,29 @@ function getNews() {
             var result = JSON.parse(response);
             $.each(result['response']['items'],function(index, value){
                 if (value['text'].length !== 0) {
+                    var cardAttachments = '<p class="card-text">';
+                    $.each(value['attachments'], function( index, value ){
+                        var type = value['type'];
+                        cardAttachments += '<p>Attachment Type: '+type+'</p>';
+                        switch (type) {
+                            case 'link':
+                                cardAttachments += '<p><a href="'+value['link']['url']+'">'+value['link']['url']+'</a></p>';
+                                break;
+                            case 'photo':
+                                cardAttachments += '<p><img src="'+value['photo']['photo_604']+'"></p>';
+                                break;
+                        }
+                        //cardAttachments += '<p>===</p>';
+                    });
+                    cardAttachments += '</p>';
                     var b = getGroupID(Math.abs(value['source_id']));
-                    //console.log(b);
+                    //var b = '0';
                     $('.cardContainer').append('<div class="card cardDecor semi-transparent">\n' +
                         '    <div class="card-body">\n' +
                         '        <h5 class="card-title">' + b + '</h5>\n' +
                         '        <p class="card-text">' + value['text'] + '</p>\n' +
                         '        <p class="card-text"><i data-feather="thumbs-up"></i> ' + value['likes']['count'] + ' &nbsp;&nbsp;&nbsp;<i data-feather="send"></i> ' + value['reposts']['count'] + ' &nbsp;&nbsp;&nbsp;<i data-feather="message-square"></i> ' + value['comments']['count'] + '</p>\n' +
+                        cardAttachments +
                         '    </div>\n' +
                         '</div>');
                 }
