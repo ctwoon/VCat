de60eaf8539dd5fd90c1c1cb61901a8d00ba8e2a
Dmytro Tarasenko <itaysontesterlab@gmail.com>
2018-03-11 21:53:16 +0200

    Auth: encode password

--- assets/js/vcat_login.js
+++ assets/js/vcat_login.js
@@ -1,22 +1,9 @@
 var url;
 var debug = false;
-function makeid() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-}
-
-console.log(makeid());
-
 $(".loginButton").click(function() {
     var username = $(".loginName").val();
     var userpass = $(".loginPass").val();
-    url = "https://oauth.vk.com/token?grant_type=password&device_id="+makeid()+"&libverify_support=1&client_id=2274003&client_secret=hHbZxrka2uZ6jB1inYsH&username=" + username + "&password=" + userpass + "&v=5.73&2fa_supported=1";
-	console.log(url);
+    url = "https://oauth.vk.com/token?grant_type=password&client_id=2274003&client_secret=hHbZxrka2uZ6jB1inYsH&username=" + username + "&password=" + encodeURIComponent(userpass) + "&v=5.73&2fa_supported=1";
     if (!debug) {
         url = "proxy.php?url=" + encodeURIComponent(url).replace(/'/g, "%27").replace(/"/g, "%22");
     } else {
@@ -26,12 +13,12 @@ $(".loginButton").click(function() {
         url: url
     }).done(function(data) {
         setItem("authToken", JSON.parse(data)['access_token']);
-        //window.location.href = 'main.html';
+        window.location.href = 'main.html';
     });
-/**
-	$('.loginLink').attr('href', url);
-    $('.loginLink').html('Перейти! (в новой вкладке)');
-    $('#loginModal').modal();**/
+    /**
+     $('.loginLink').attr('href', url);
+     $('.loginLink').html('Перейти! (в новой вкладке)');
+     $('#loginModal').modal();**/
 });
 
 /**$(".captchaOpen").click(function() {
@@ -42,13 +29,13 @@ $(".loginButton").click(function() {
     $('.captchaLink').hide();
 });
 
-$(".captchaGet").click(function() {
+ $(".captchaGet").click(function() {
     $('.captchaImage').attr('src', $('.captchaURL').val());
     $('.captchaImage').show();
     $('.captchaKey').show();
 });
 
-$(".captchaGetURL").click(function() {
+ $(".captchaGetURL").click(function() {
     var key = $(".captchaKey").val();
     var sid = $(".captchaID").val();
     var aurl = url + encodeURIComponent("&captcha_sid="+sid+"&captcha_key="+key);
