eb2022272ae1b00746e2642c4707c74baa69fac4
iTaysonLab <itaysontesterlab@gmail.com>
2018-04-01 21:41:08 +0300

    123

--- assets/js/vcat/GroupList.js
+++ assets/js/vcat/GroupList.js
@@ -5,7 +5,7 @@ function getGroups() {
     $.ajax({
         url: url,
         success: function( response ) {
-          logInfo("GroupList", "Got GroupList JSON");
+            logInfo("GroupList", "Got GroupList JSON");
             var result = JSON.parse(response);
             logInfo(response);
             $.each(result['response']['items'],function(index, value){
@@ -22,6 +22,34 @@ function getGroups() {
                 getUser($(this).attr('vcat-userid'));*/
             });
             logInfo("GroupList", "Finish GroupList");
+        }
+    });
+}
+
+function getGroupInfo(groupID) {
+    logInfo("GroupInfo", "Get GroupInfo");
+    var url = "https://api.vk.com/method/groups.get?user_id="+user_id+"&extended=1&access_token="+token+"&v=5.73&count=999";
+    url = craftURL(url);
+    $.ajax({
+        url: url,
+        success: function( response ) {
+            logInfo("GroupInfo", "Got GroupInfo JSON");
+            var result = JSON.parse(response);
+            logInfo(response);
+            $.each(result['response']['items'],function(index, value){
+                $('.cardContainer').append('<div class="card cardDecor semi-transparent message messageBorder showGroup" vcat-groupid="'+value['id']+'">\n' +
+                    '    <div class="card-body messagePadding">\n' +
+                    '        <p class="card-text">' + value['name'] + '</p>\n' +
+                    '    </div>\n' +
+                    '</div>');
+            });
+            feather.replace();
+            $('.spinnerLoad').hide();
+            $(".showUser").click(function () {
+                /*logError($(this).attr('vcat-userid'));
+                getUser($(this).attr('vcat-userid'));*/
+            });
+            logInfo("GroupInfo", "Finish GroupInfo");
         }
     });
 }
