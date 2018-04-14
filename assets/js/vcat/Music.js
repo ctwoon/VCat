d5e6614fb1484d21933afe4a6e4758f827af265f
iTaysonLab <itaysontesterlab@gmail.com>
2018-04-15 00:55:20 +0300

    shhhh, we have music (beta quality, not working)

--- assets/js/vcat/Music.js
+++ assets/js/vcat/Music.js
@@ -3,12 +3,27 @@ function getMusic() {
     var url;
     var offset = 0;
     url = craftMethodURL('execute', 'getMusicPage', 'owner_id='+getItem("userId")+"&need_owner=1&need_playlists=1&playlists_count=12&audio_offset="+offset+"&audio_count=100", '5.74');
-    logInfoNew("Requesting url: "+url);
+    logInfo("Music", "Requesting url: "+url);
     $.ajax({
         url: url,
         success: function( response ) {
             $('.loading').hide();
-            logInfoNew("Got audio: "+response);
+            logInfo("Music", "Got audio list: "+response);
+            var result = JSON.parse(response);
+            if (result['response']['playlists'] == false) {
+                logInfo("Music", "Error 25, requesting a token...");
+                getToken();
+            }
+        }
+    });
+}
+
+function getToken() {
+    var url = "libvkmusic/index.php";
+    $.ajax({
+        url: url,
+        success: function( response ) {
+            console.log(response);
         }
     });
 }
